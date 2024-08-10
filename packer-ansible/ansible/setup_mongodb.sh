9d3a0cb350c6aa9606582465849a3bd73a5c0457
nnyore <124398515+nnyore@users.noreply.github.com>
2024-08-10 22:52:02 +0100

    Sat 10 Aug 2024 22:52:02 IST
    Update

--- packer-ansible/ansible/setup_mongodb.sh
+++ packer-ansible/ansible/setup_mongodb.sh
@@ -91,6 +91,25 @@ processManagement:
   fork: true
 EOF
 
+# Start MongoDB instances
+sudo mongod --config /etc/mongod1.conf
+sudo mongod --config /etc/mongod2.conf
+sudo mongod --config /etc/mongod3.conf
+
+# Initialize the replica set
+sudo mongosh --eval 'rs.initiate({_id: "rs0", members: [{ _id: 0, host: "localhost:27017" }, { _id: 1, host: "localhost:27018" }, { _id: 2, host: "localhost:27019" }]})' --port 27017
+
+sudo mongosh --eval 'use admin
+db.createUser({
+  user: "admin",
+  pwd: "password", // Replace with a strong password
+  roles: [{ role: "root", db: "admin" }]
+})' --port 27017
+
+
+
+
+------
 cat <<EOF | sudo tee kill_mongod.sh
 #!/bin/bash
 
@@ -104,11 +123,3 @@ echo "mongod stooped."
 EOF
 
 chmod +x kill_mongod.sh
-
-# Start MongoDB instances
-sudo mongod --config /etc/mongod1.conf
-sudo mongod --config /etc/mongod2.conf
-sudo mongod --config /etc/mongod3.conf
-
-# Initialize the replica set
-sudo mongosh --eval 'rs.initiate({_id: "rs0", members: [{ _id: 0, host: "localhost:27017" }, { _id: 1, host: "localhost:27018" }, { _id: 2, host: "localhost:27019" }]})' --port 27017
