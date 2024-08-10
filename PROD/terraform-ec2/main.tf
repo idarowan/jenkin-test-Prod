9a0fea303872d6ba247999d04c63e34a8fb4521d
nnyore <124398515+nnyore@users.noreply.github.com>
2024-08-10 18:13:10 +0100

    Sat Aug 10 18:13:10 IST 2024
    Updated

--- PROD/terraform-ec2/main.tf
+++ PROD/terraform-ec2/main.tf
@@ -1,11 +1,12 @@
 resource "aws_instance" "example" {
-  count         = 5
   ami           = var.ami_id
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.allow_traffic.id]
+  subnet_id     = aws_subnet.publicsubnet.id
   key_name      = var.key_name
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "Public-Jenkins-EC2"
   }
 }
 
